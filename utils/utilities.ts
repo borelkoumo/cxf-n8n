820b958936a11f116329110e50cc6398e4cb6909
Borel KOUMO <borel.koumo_ext@michelin.com>
2025-08-12 18:40:44 +0200

    Added readme

---
+++ utils/utilities.ts
@@ -0,0 +1,10 @@
+import type {
+	IPairedItemData,
+} from 'n8n-workflow';
+
+
+export function generatePairedItemData(length: number): IPairedItemData[] {
+	return Array.from({ length }, (_, item) => ({
+		item,
+	}));
+}
